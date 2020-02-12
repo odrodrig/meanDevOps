1c399236c420df713dd27d870666ba8c1a778a69
Oliver Rodriguez <odrodrig@us.ibm.com>
2020-02-12 06:34:35 -0600

    fixing code coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/supply-store'),
-      reports: ['html', 'lcovonly', 'text-summary', 'json', 'lcov'],
+      reports: ['html', 'lcovonly', 'text-summary', 'json', 'lcov', 'json-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
