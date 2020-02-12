9e3fb23384388b643cde7938cbb14b78f3b6519e
Oliver Rodriguez <odrodrig@us.ibm.com>
2020-02-12 06:50:00 -0600

    Added deployment upload

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 cf push "${CF_APP}"
+
+
+EXIT=$?
+if [ $EXIT -eq 0 ]; then STATUS=pass; else STATUS=fail; fi;
+
+
+ibmcloud plugin install -f doi
+ibmcloud login --apikey $API_KEY --no-region
+ibmcloud doi publishdeployrecord --logicalappname="odr-mean-demo" --buildnumber="$BUILD_NUMBER" --env="prod" --status=$STATUS
+
+
+exit $EXIT
